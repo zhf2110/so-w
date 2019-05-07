e32fd69ae508831209133c482485809668e19ce5
张峰 <zhangfeng542@ljs.local>
2019-05-07 17:58:48 +0800

    shori学习

---
+++ sso-common/src/main/java/com/so/w/constant/SSOConstant.java
@@ -0,0 +1,15 @@
+package com.so.w.constant;
+
+/**
+ * @author zhangfeng542
+ * @date 5/7/19
+ */
+public class SSOConstant {
+
+	public final static String TOKEN_NAME = "token";
+
+	public final static String COOKICE_NAME = "sow_cookiceID";
+
+	public final static String CLIENT_SERVICE = "service";
+
+}
---
+++ sso-common/src/main/java/com/so/w/filter/SSOBaseFilter.java
@@ -0,0 +1,29 @@
+package com.so.w.filter;
+
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author zhangfeng542
+ * @date 5/7/19
+ */
+public class SSOBaseFilter extends HttpServlet implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+		throws IOException, ServletException {
+
+	}
+}
